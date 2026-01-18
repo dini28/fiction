9ba469fd4fbef9ec5624acfa9adb72c5ecf30dd6
dini28 <dipeshsonitech@gmail.com>
2026-01-18 18:59:47 +0530

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { TextPlugin } from 'gsap/TextPlugin';
@@ -7,19 +7,20 @@ import Footer from "./components/layout/Footer/Footer"
 import Header from "./components/layout/Header/Header"
 import Loader from "./components/ui/Loader/Loader";
 import SmoothScroll from "./components/ui/SmoothScroll/SmoothScroll";
-import Home from './pages/Home';
-import AboutPage from './pages/AboutPage';
-import CareersPage from './pages/CareersPage';
-import NewsPage from './pages/NewsPage';
-import LoginPage from './pages/LoginPage';
-import ArmoryPage from './pages/ArmoryPage';
-import NotFoundPage from './pages/NotFoundPage';
-
 import GlobalNoise from "./components/ui/GlobalNoise/GlobalNoise";
 import CustomCursor from "./components/ui/CustomCursor/CustomCursor";
 
 import gsap from 'gsap';
 gsap.registerPlugin(ScrollTrigger, TextPlugin, useGSAP);
+
+// Lazy Load Pages
+const Home = lazy(() => import('./pages/Home'));
+const AboutPage = lazy(() => import('./pages/AboutPage'));
+const CareersPage = lazy(() => import('./pages/CareersPage'));
+const NewsPage = lazy(() => import('./pages/NewsPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const ArmoryPage = lazy(() => import('./pages/ArmoryPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -47,15 +48,19 @@ function AppContent() {
       <CustomCursor />
       {isLoading && <Loader onComplete={() => setIsLoading(false)} />}
       {showLayout && <Header />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/careers" element={<CareersPage />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/armory" element={<ArmoryPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+
+      <Suspense fallback={<div style={{ height: '100vh', background: '#0a0a0f' }}></div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/careers" element={<CareersPage />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/armory" element={<ArmoryPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+
       {showLayout && <Footer />}
     </SmoothScroll>
   );
